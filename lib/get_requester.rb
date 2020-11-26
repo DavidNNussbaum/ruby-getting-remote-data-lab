d6db61bd68c6e28bfd4428b6a01a020094375c24
DavidNNussbaum <brocknynj@aol.com>
2020-11-26 10:12:47 -0500

    commit tree 2a286814001a7f647fda1904f5c9648cde71e54d

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,49 @@
-# Write your code here
+# require 'net/http'
+# require 'open-uri'
+# require 'json'
+
+
+# class GetRequestor
+#     # URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+#     def initialize(url)
+#         @url = url
+#     end
+    
+#     def get_response_body
+#          uri = URI.parse(@url)
+#          response = Net::HTTP.get_response(uri)
+#          response.body
+#     end
+
+#     def parse_json
+#         JSON.parse(get_response_body)
+#         # programs.collect do |data|
+#         # data["info"] 
+#     # end 
+#     end
+# end
+
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
+
+
+
+  
